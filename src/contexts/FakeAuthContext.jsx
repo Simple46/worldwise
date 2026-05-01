6de0ce500f474a3faeb3c58c764567a746cb69a8
imple46 <olayiwolahissa484@gmail.com>
2026-05-01 00:57:27 -0700

    update authentication context

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -8,8 +8,8 @@ const initialState = {
 };
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Hissa",
+  email: "hissa@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
